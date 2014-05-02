c38292ae43e77b44b1ff5ff564cd442b83840b7c
dmitry.khorevich <dmitry.khorevich@altoros.com>
2014-05-02 13:13:30 +0300

    redactoring

--- src/main/java/com/kharevich/HelloWorld.java
+++ src/main/java/com/kharevich/HelloWorld.java
@@ -3,7 +3,10 @@ package com.kharevich;
 import com.kharevich.domain.User;
 import com.kharevich.repository.Repository;
 import com.kharevich.repository.RepositoryImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -18,11 +21,20 @@ import static javax.persistence.Persistence.createEntityManagerFactory;
 @Path("hello")
 public class HelloWorld {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+
+    private Repository repo;
+
+    @PostConstruct
+    public void init() {
+        logger.info("run init");
+        EntityManagerFactory emf = createEntityManagerFactory(Constants.MYSQL);
+        repo = new RepositoryImpl(emf);
+    }
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        EntityManagerFactory emf = createEntityManagerFactory(Constants.MYSQL);
-        Repository repo = new RepositoryImpl(emf);
         User user = new User();
         user.setName("Name");
         user.setSurname("Surname");
